30002fc764b2d7141df79284964db915970ab34e
Myles Lake <lakemyles@gmail.com>
2022-07-30 15:55:44 -0500

    Add notes on Big O time complexity

---
+++ big-o/time-complexity.js
@@ -0,0 +1,26 @@
+// Big O Notation - Time Complexity
+// Numeric representation of the performance of code
+// Big O(1) - Constant time
+// Big O(n) - Linear time
+// Big O(n^2) - Quadratic time
+// Big O(nlog(n)) - Logarythmic time
+// Big O(log(n)) - Logarythmic time
+
+// Big O(n) - Linear time
+function addUpToLinearTime(n) {
+    let total = 0;
+    for(let i = 1; i <=n; i++) {
+        total += i;
+    }
+    return total;
+}
+
+// Big O(1) - Constant time
+function addUpToConstantTime(n) {
+    return n * (n + 1) / 2;
+}
+
+// Print answer
+let answer = addUpTo(4);
+console.log(answer);
+
