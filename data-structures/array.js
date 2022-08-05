1aca8bc1147dcc4a9c9f9f4d908da2ce03cc5476
Myles Lake <lakemyles@gmail.com>
2022-08-05 15:44:25 -0500

    Add notes on Big O of arrays

---
+++ data-structures/array.js
@@ -0,0 +1,33 @@
+// Arrays
+// Ordered lists!
+// When to use
+    // You need order
+    // When you need fast access/insertion/removal... sort of
+
+// If you have an index it is constant lookup time (if the index is within the range of the array)
+// Inserting/removing at the end of the array is constant time
+// Inserting/removing at the beginning of the array is linear time - We have to reindex each element in the array
+
+
+// Big O
+// -----
+// Insertion - 
+    // End - O(1)
+    // Beginning - O(n)
+// Removal - 
+    // End - O(1)
+    // Beginning - O(n)
+// Searching - O(n)
+// Access - O(1)
+
+
+// Array methods
+    // push - O(1)
+    // pop - O(1)
+    // shift - O(n)
+    // unshift - O(n)
+    // sort - O(nlogn)
+    // forEach/map/filter/reduce - O(n)
+    // concat/slice/splice - O(n)
+
+
--- data-structures/object.js
+++ data-structures/object.js
@@ -4,6 +4,15 @@
     // You need fast access / insertion / removal - All are constant time
     // Searching is linear time
 
+    
+// Big O
+// -----
+// Insertion - O(1)
+// Removal - O(1)
+// Searching - O(n)
+// Access - O(1)
+
+
 // Object.keys() - linear time
 // Object.values() - linear time
 // Object.entries() - linear time
